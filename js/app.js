c0513385037e1d46e989e5cbcc3d059e5a4a6105
taha mahdy <you@example.com>
2024-03-02 16:14:36 +0330

    frst commit

---
+++ js/app.js
@@ -0,0 +1,72 @@
+// let name = prompt("what is your name");
+// let lastname = prompt("what is your last name");
+// let Age = prompt("how old are you");
+// let programming = prompt("What programming language are you learning?");
+// let teacher = prompt("Who is your teacher?");
+// let classactivity = prompt("What was the grade of your class activity?");
+// let Hondout = prompt("What was the grade of your Hondout?");
+// let Exam = prompt("What was the grade of your Eaxm?");
+// let Overal = prompt("What was the grade of your Overal?");
+let classrom = [
+  Ardani = {
+    name: Amir-Hossin,
+    lestname:Ardani,
+    Age:14,
+    teacher:hossin-eskandari,
+    classactivity:-C,
+    Hondout:-C,
+    Exam:-C,
+    Overal:-C,
+  },
+  alizahdi = {
+    name: Amir-ali,
+    lestname:zahdi,
+    Age:15,
+    teacher:hossin-eskandari,
+    classactivity:-B,
+    Hondout:+C,
+    Exam:+C,
+    Overal:+C,
+  },
+  Arshiazahdi ={
+    name: Arshia,
+    lestname: zahdi,
+    Age:14,
+    teacher:hossin-eskandari,
+    classactivity:-B,
+    Hondout:-B,
+    Exam:+C,
+    Overal:-B,
+  },
+  tahaZanghaneh ={
+    name:mahamd-taha,
+    lestname: Zanghaneh,
+    Age:12,
+    teacher:hossin-eskandari,
+    classactivity:-B,
+    Hondout:+B,
+    Exam:-A,
+    Overal:+B,
+  },
+  mahdiZanghaneh ={
+    name:mahamd-mahdi,
+    lestname: Zanghaneh,
+    Age:14,
+    teacher:hossin-eskandari,
+    classactivity:+B,
+    Hondout:-B,
+    Exam:+B,
+    Overal:+B,
+  },
+  Fakhri ={
+    name:Pouya,
+    lestname: Fakhri,
+    Age:14,
+    teacher:hossin-eskandari,
+    classactivity:+A,
+    Hondout:+B,
+    Exam:-A,
+    Overal:-A,
+  },
+];
+console.log(classrom[0].Age)
